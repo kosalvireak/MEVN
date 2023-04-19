9734013f79cb9ab13029e908edc9122293115d87
Kosalvireak Khoeun <virakvary@gmail.com>
2023-04-19 11:09:57 +0700

    Test: Push obj to mongo and retrieve

---
+++ backend/data/test.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require('mongodb');
+
+const name = ["kosalvireak", "Monyjenni", "Punvireakroth", "Chanjnint21", "heatkoemnak", "SokreaksaRoeurng", "chakriyamet", "NhorkSopheakna", "SOYHOK", "Ponhrith", "nohannah", "Pheak02", "soumany", "Neangthary", "phearaZ", "Sreynaj", "RaThida", "SIPHAI", "Hok"];
+const number = [330, 64, 337, 59, 78, 54, 46, 43, 73, 296, 131, 24, 117, 25, 51, 175, 32, 65, 500];
+const data = {
+    "identify": "data1",
+    name,
+    number
+}
+
+let myCollection;
+
+async function main() {
+    const client = await MongoClient.connect('mongodb://127.0.0.1:27017', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = client.db('mydatabase');
+    myCollection = db.collection('mycollection');
+    console.log("Mongo connection Open");
+    return myCollection;
+}
+main().catch(err => console.log(err));
+
+
+async function insertDocuments() {
+    const collection = await main();
+    const result = await collection.insertOne(data);
+    console.log(result);
+}
+
+let result;
+async function showDocument() {
+    const collection = await main();
+    result = await collection.find().toArray();
+    console.log(result);
+    console.log(result[0].name);
+}
+
+
+
+// insertDocuments();
+showDocument();
+// console.log(result);
---
+++ frontend/src/data/json.js
@@ -0,0 +1,22 @@
+const data = [
+    { name: 'kosalvireak', number: '330' },
+    { name: 'Monyjenni', number: '64' },
+    { name: 'Punvireakroth', number: '337' },
+    { name: 'Chanjnint21', number: '59' },
+    { name: 'heatkoemnak', number: '78' },
+    { name: 'SokreaksaRoeurng', number: '54' },
+    { name: 'chakriyamet', number: '46' },
+    { name: 'NhorkSopheakna', number: '43' },
+    { name: 'SOYHOK', number: '73' },
+    { name: 'Ponhrith', number: '296' },
+    { name: 'nohannah', number: '131' },
+    { name: 'Pheak02', number: '24' },
+    { name: 'soumany', number: '117' },
+    { name: 'Neangthary', number: '25' },
+    { name: 'phearaZ', number: '51' },
+    { name: 'Sreynaj', number: '175' },
+    { name: 'RaThida', number: '32' },
+    { name: 'SIPHAI', number: '65' },
+    { name: 'Hok', number: '500' }
+];
+module.exports = data;
